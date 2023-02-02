b184c4315df146c4d356c878fe356800d0737de4
haind <nguyenduonghai07@gmail.com>
2023-02-02 17:53:19 +0700

    delete old véion

--- homepage/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Search(models.Model):
-    img = models.ImageField(upload_to = "images/upload", blank=True)
-    topk = models.IntegerField(default=10)
